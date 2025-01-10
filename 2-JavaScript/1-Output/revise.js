b8007519306af57d1a5ac97b9d0274b9f2d03d4d
Arsalan Asad <arsalan.gazdar@gmail.com>
2025-01-10 17:46:31 +0500

    CSS and JavaScript update

---
+++ 2-JavaScript/1-Output/revise.js
@@ -0,0 +1,84 @@
+// let a = 25
+// let b = 50
+// let c = ['one', '2', 'three', 'four']
+
+// console.log(c)
+// c.push('gulkhan', 'fareed')
+// console.log(c)
+// c.pop()
+// console.log(c)
+// c.unshift('huzaifa', 'abdullah')
+// console.log(c)
+// c.shift()
+// console.log(c)
+// c.splice(2, 2, 'jawednihar')
+// console.log(c)
+// let fruit = c.slice(1, 15)
+// console.log(c, fruit)
+
+// let a = 20
+// var arry = []
+// for (var i = 0; i < 10; i++) {
+//     console.log(i)
+// }
+// alert ('hello world')
+
+// var arry = ['Mubeen', 'Alam', 'Zohaib', 'Adam', 'Mansoor']
+// for (var i = 0; i < arry.length; ++i) {
+//     console.log(arry[i])
+// }
+
+// slice, charAt, indexOf, lastIndexOf, replace, replaceAll
+// Math .random, .ceil, .floor, .round
+
+// let city = 'afganistan'
+
+// console.log(city.slice(0, 3))
+// console.log(city.charAt(5))
+// console.log([6])
+// console.log(city.indexOf('t'))
+// console.log(city.lastIndexOf('a'))
+// console.log(city.replace('A', 'a'))
+// console.log(city.replaceAll('a', 'A'))
+
+
+// MATCH, ROUND, CEIL, FLOOR
+// let bill = 15.7766666
+// let newBill = Math.round(bill)
+// console.log(newBill)
+
+// let newBill1 = Math.ceil(bill)
+// console.log(newBill1)
+
+// let newBill2 = Math.floor(bill)
+// console.log(newBill2)
+
+
+//RANDOM NUMBER GENERATION
+// let random = Math.random()
+// console.log(random.toFixed(2))
+// console.log(random)
+
+// let highest = Math.random()*10
+// let lowest = 0.0000000000000001 * 2
+// console.log(Math.round(highest))
+// console.log(lowest)
+
+// DATE
+// let date = new Date()
+
+// console.log(typeof date.toTimeString())
+// console.log(date.getDay())
+// console.log(date.getDate())
+// console.log(date.getMonth())
+// console.log(date.getFullYear())
+// console.log(date.getMilliseconds())
+// console.log(date.getSeconds())
+// console.log(date.getMinutes())
+// console.log(date.getHours())
+
+const bdDate = new Date("6 Dec 2025");
+bdDate.setFullYear("2026");
+bdDate.setMonth("");
+
+console.log(bdDate)
