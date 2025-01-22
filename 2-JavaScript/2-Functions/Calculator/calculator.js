790bf561e3ba45c6c8e8ca588fb100a1b960f05a
Arsalan Asad <arsalan.gazdar@gmail.com>
2025-01-22 17:37:31 +0500

    Calculator Created

---
+++ 2-JavaScript/2-Functions/Calculator/calculator.js
@@ -0,0 +1,23 @@
+const hcd = document.querySelector ('#hcd')
+
+function writeOperation (newValue) {
+    const prevValue = hcd.innerHTML
+    hcd.innerHTML = prevValue + newValue
+}
+
+function performCalculation () {
+    const values = hcd.innerHTML
+    const result = eval(values)
+    hcd.innerHTML = result
+    console.log (result)
+}
+
+function clearAll () {
+    const defaultValue = ''
+    hcd.innerHTML = defaultValue
+}
+
+function clearLastValue () {
+    const lastValue = hcd.innerHTML
+    hcd.innerHTML = lastValue.slice(0, -1)
+}
