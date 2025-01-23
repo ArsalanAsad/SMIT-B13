07baa759adbe01c159f1fe575453ccd750180c13
Arsalan Asad <arsalan.gazdar@gmail.com>
2025-01-23 16:38:33 +0500

    CSS Design and responsiveness added in the Dice Game

---
+++ 2-JavaScript/2-Functions/randomNumGame/withMediaQuery/game.app.js
@@ -0,0 +1,36 @@
+let score = 0
+const options = [1, 2, 3, 'Exit', 4, 5, 6]
+
+function addPoints (option) {
+    const scoreElement = document.getElementById ('currentScore')
+    const totalScoreElement = document.getElementById ('score')
+    const highScoreElement = document.getElementById ('highScore')
+    const previousScore = +scoreElement.innerText
+    
+    if (option === "Exit") {
+        if (+highScoreElement.innerText < scoreElement.innerText) {
+            highScoreElement.innerText = scoreElement.innerText 
+        }
+        totalScoreElement.innerText = +totalScoreElement.innerText + previousScore;
+        scoreElement.innerText = 0
+    } else {
+        scoreElement.innerText = option + previousScore
+    }
+}
+
+function getOption () {
+    let randomNumber = Math.random()
+    randomNumber = randomNumber * 6
+    randomNumber = Math.round(randomNumber)
+
+    const option = options[randomNumber]
+    return option
+}
+
+function showPoint() {
+    const option = getOption()
+    addPoints(option)
+
+    const diceElement = document.getElementById('dice')
+    diceElement.innerText = option
+}
