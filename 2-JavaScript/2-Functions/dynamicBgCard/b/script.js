cbfca8f1dfdf8a6090c7083103e683c0e22e89a1
Arsalan Asad <arsalan.gazdar@gmail.com>
2025-02-21 18:00:06 +0500

    Dom oriented UserDefinedCar.

--- 2-JavaScript/2-Functions/dynamicBgCard/b/script.js
+++ 2-JavaScript/2-Functions/dynamicBgCard/b/script.js
@@ -1,10 +1,13 @@
-document.addEventListener("DOMContentLoad", function() {
+document.addEventListener("DOMContentLoaded", function() {
     const title = prompt("Enter the product title");
     const description = prompt("Enter the product description");
-    const buttonTitle = prompt("Enter the button title");
+    const buttonTitle = prompt("Enter the button title:");
     const buttonLink = prompt("Enter the button link (absolute URL):");
     const imageLink = prompt("Enter the image link (absolute URL):");
 
     document.getElementById("product-title").textContent = title;
-    document.getElementById("product-description").textContent
+    document.getElementById("product-description").textContent = description;
+    document.getElementById("product-button").textContent = buttonTitle;
+    document.getElementById("product-button").href = buttonLink;
+    document.getElementById("product-image").src = imageLink;
 })
