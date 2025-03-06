1b46056f8e8ba077c099fda2446b8aece72c330d
Arsalan Asad <arsalan.gazdar@gmail.com>
2025-03-06 13:01:26 +0500

    destructuring

--- 2-JavaScript/4-SocialMedia/app.js
+++ 2-JavaScript/4-SocialMedia/app.js
@@ -12,3 +12,33 @@ async function fetchPostData() {
     }
 }
 fetchPostData()
+function createCard(title, body) {
+    const cardUI = 
+    `<div class="card">
+    <p>${title}</p>
+    <p>${body}</p>
+    </div>`
+
+    mainWrapper.innerHTML += cardUI
+}
+
+async function submitPost(event) {
+    event.preventDafault()
+
+    const title = document.getElementById('title').value;
+    const body = document.getElementById('body').value;
+    const userId = document.getElementById('userId').value;
+    const payload = { title, body, userid }
+    const response = await fetch('https://jsonplaceholder.typicode.com/posts', {
+        method: 'POST',
+        BODY: JSON.stringify(payload),
+        headers: {
+            'Content-type': 'application/json; charset=UTF-8',
+        },
+    })
+
+    const data = await response.json()
+
+    console.log(data, "submit")
+}
+postForm.addEventListener('submit', submitPost)
