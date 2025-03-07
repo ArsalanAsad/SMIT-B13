193b77e6a6031c10fd957130603b9b31b4bd5b47
Arsalan Asad <arsalan.gazdar@gmail.com>
2025-03-07 15:26:50 +0500

    use asynch function for improving code

--- 2-JavaScript/4-SocialMedia/app.js
+++ 2-JavaScript/4-SocialMedia/app.js
@@ -22,6 +22,27 @@ function createCard(title, body) {
     mainWrapper.innerHTML += cardUI
 }
 
+// async function submitPost(event) {
+//     event.preventDafault()
+
+//     const title = document.getElementById('title').value;
+//     const body = document.getElementById('body').value;
+//     const userId = document.getElementById('userId').value;
+//     const payload = { title, body, userid }
+//     const response = await fetch('https://jsonplaceholder.typicode.com/posts', {
+//         method: 'POST',
+//         BODY: JSON.stringify(payload),
+//         headers: {
+//             'Content-type': 'application/json; charset=UTF-8',
+//         },
+//     })
+
+//     const data = await response.json()
+
+//     console.log(data, "submit")
+// }
+// postForm.addEventListener('submit', submitPost)
+
 async function submitPost(event) {
     event.preventDafault()
 
