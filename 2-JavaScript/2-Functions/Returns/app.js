e7d56d3d8c0061e5bfd58bf70b5615d3cb9aff7c
Arsalan Asad <arsalan.gazdar@gmail.com>
2025-05-19 16:54:49 +0500

    Creating function

---
+++ 2-JavaScript/2-Functions/Returns/app.js
@@ -0,0 +1,49 @@
+// let x = myFunction (4, 3)
+// console.log(x);
+// document.getElementById("demo").innerHTML = x;
+// function myFunction(a, b) {
+//     return a * b;
+// };
+
+// function toCelsius(fahrenheit) {
+//     return (5/9) * (fahrenheit-32);
+// }
+
+// let value = toCelsius(77);
+// document.getElementById("demo").innerHTML = value;
+// console.log(value);
+
+// function toCelsius(f) {
+//   return (5/9) * (f-32);
+// }
+
+// let value = toCelsius();
+// document.getElementById("demo").innerHTML = value;
+// console.log(value)
+
+// function toCelsius(fahrenheit) {
+//   return (5/9) * (fahrenheit-32);
+// }
+
+// // let value = toCelsius;
+// // console.log(value);
+
+// let x = toCelsius(77);
+// // let text = "The temperature is " + x + " Celsius";
+// console.log(x);
+// // console.log(text);
+
+// let text = "The temperature is " + toCelsius(77) + " Celsius";
+// console.log(text);
+
+// code here can NOT use carName
+
+function myFunction() {
+  let carName = "Volvo";
+  // code here CAN use carName
+  console.log(carName)
+}
+myFunction();
+
+let carName = "alto"
+console.log(carName)
