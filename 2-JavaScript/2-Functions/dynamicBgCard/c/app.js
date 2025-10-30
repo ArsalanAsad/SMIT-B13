ca6efb94b617781f62d5b831fffcc6efcc3bbb39
Arsalan Asad <arsalan.gazdar@gmail.com>
2025-10-30 12:51:49 +0500

    Responsive Card

---
+++ 2-JavaScript/2-Functions/dynamicBgCard/c/app.js
@@ -0,0 +1,13 @@
+document.addEventListener ("DOMContentLoaded", function () {
+    const title = prompt ("Enter the product title");
+    const description = prompt ("Enter the product description");
+    const buttonTitle = prompt ("Enter the button title:");
+    const buttonLink = prompt ("Enter the button link (absolute URL):");
+    const imageLink = prompt ("Enter the image link (absolute URL):");
+
+    document.getElementById("product-title").textContent = title;
+    document.getElementById("product-description").textContent = description;
+    document.getElementById("product-button").textContent = buttonTitle
+    document.getElementById("product-button").href = buttonLink
+    document.getElementById("product-image").src = imageLink
+})
