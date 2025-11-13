af52d894b6f301e8d0a5c7521dbdb80df6136ff7
Arsalan Asad <arsalan.gazdar@gmail.com>
2025-11-13 16:14:16 +0500

    Modal completed

--- 2-JavaScript/0-Revision/1classRiv/app.js
+++ 2-JavaScript/0-Revision/1classRiv/app.js
@@ -43,27 +43,39 @@
 //console.log(height);    //  ->  180
 //console.log(weight);    //  ->  Uncaught  ReferenceError:  weight  is  not  defined
 
-var  height  =  180;
-console.log(height);    //  ->  180
-console.log(weight);    //  ->  undefined
-var  weight  =  70;
-console.log(weight);    //  ->  70
+// var  height  =  180;
+// console.log(height);    //  ->  180
+// console.log(weight);    //  ->  undefined
+// var  weight  =  70;
+// console.log(weight);    //  ->  70
 
-// there are many possible and correct variable names
-let rosePrice = 8;
-let lilyPrice = 10;
-let tulipPrice = 2;
+// // there are many possible and correct variable names
+// let rosePrice = 8;
+// let lilyPrice = 10;
+// let tulipPrice = 2;
 
-let numberOfRoses = 70;
-let numberOfLilies = 50;
-let numberOfTulips = 120;
+// let numberOfRoses = 70;
+// let numberOfLilies = 50;
+// let numberOfTulips = 120;
 
-let rosesValue = rosePrice * numberOfRoses;
-let liliesValue = lilyPrice * numberOfLilies;
-let tulipsValue = tulipPrice * numberOfTulips;
+// let rosesValue = rosePrice * numberOfRoses;
+// let liliesValue = lilyPrice * numberOfLilies;
+// let tulipsValue = tulipPrice * numberOfTulips;
 
-let total = rosesValue + liliesValue + tulipsValue;
-console.log("Rose – unit price:", rosePrice, ", quantity:", numberOfRoses, ", value:", rosesValue);
-console.log("Lily – unit price:", lilyPrice, ", quantity:", numberOfLilies, ", value:", liliesValue);
-console.log("Tulip – unit price:", tulipPrice, ", quantity:", numberOfTulips, ", value:", tulipsValue);
-console.log("Total: ", total);
+// let total = rosesValue + liliesValue + tulipsValue;
+// console.log("Rose – unit price:", rosePrice, ", quantity:", numberOfRoses, ", value:", rosesValue);
+// console.log("Lily – unit price:", lilyPrice, ", quantity:", numberOfLilies, ", value:", liliesValue);
+// console.log("Tulip – unit price:", tulipPrice, ", quantity:", numberOfTulips, ", value:", tulipsValue);
+// console.log("Total: ", total);
+
+let num = 1
+
+function hWorld () {
+     if (num > 3) {
+        alert ('number is Greater then 3')
+     } else {
+        alert ('hellow world')
+     }
+}
+
+hWorld();
